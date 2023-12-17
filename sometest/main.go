5ffc4632598b6a9f206291a49edbe5ede296f9c1
aaaasmile <testhaus@gmx.at>
2023-12-17 18:06:43 +0100

    moved into web

--- sometest/main.go
+++ sometest/main.go
@@ -1,74 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-	"net/url"
-	"strings"
-	"text/template"
-	"time"
+
+	"github.com/aaaasmile/crawler/sometest/web"
 )
-
-type PageCtx struct {
-	Buildnr string
-}
-
-const (
-	buildnr = "00.00.10.00"
-)
-
-func handleGet(w http.ResponseWriter, req *http.Request) error {
-	var err error
-	u, _ := url.Parse(req.RequestURI)
-	log.Println("GET requested ", u)
-	pagectx := PageCtx{
-		Buildnr: buildnr,
-	}
-
-	templName := "templates/index.html"
-
-	tmplIndex := template.Must(template.New("AppIndex").ParseFiles(templName))
-
-	err = tmplIndex.ExecuteTemplate(w, "base", pagectx)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func APiHandler(w http.ResponseWriter, req *http.Request) {
-	switch req.Method {
-	case "GET":
-		if err := handleGet(w, req); err != nil {
-			log.Println("Error on process request: ", err)
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		}
-	}
-}
 
 func main() {
+	log.Println("Testin svg scraping and conversion")
 	//scrap.Scrap()
-	serverurl := "127.0.0.1:5903"
-	rootURLPattern := "/svg/"
-	finalServURL := fmt.Sprintf("http://%s%s", strings.Replace(serverurl, "0.0.0.0", "localhost", 1), rootURLPattern)
-
-	finalServURL = strings.Replace(finalServURL, "127.0.0.1", "localhost", 1)
-	log.Println("Server started with URL ", serverurl)
-	log.Println("Try this url: ", finalServURL)
-
-	http.Handle(rootURLPattern+"static/", http.StripPrefix(rootURLPattern+"static", http.FileServer(http.Dir("static"))))
-	http.HandleFunc(rootURLPattern, APiHandler)
-
-	srv := &http.Server{
-		Addr:         serverurl,
-		WriteTimeout: time.Second * 15,
-		ReadTimeout:  time.Second * 15,
-		IdleTimeout:  time.Second * 60,
-		Handler:      nil,
-	}
-	if err := srv.ListenAndServe(); err != nil {
-		log.Println("Server is not listening anymore: ", err)
-	}
+	web.StartServer()
 }
